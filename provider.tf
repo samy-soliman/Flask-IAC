fa88b365e5b418df45054c1d7f58a870fd0a40d4
samy-soliman <samy.soliman1997@gmail.com>
2023-11-03 05:49:02 +0200

    adding remote state file, removing local SA auth and using cloud build SA , adding cloudbuild script

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = file("SA_KEY.json")
+    # credentials = file("SA_KEY.json") # no need for it in cloud build will use his SA
     project = var.project_id
     region =  var.project_region   # default for resources
 }
